77e4658f4f3b92aed3b0fb13e8005ffc2bf3d58d
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2023-03-22 17:18:37 +0300

    publish photo to Telegram channel.

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def main():
     tg_token = os.environ["TG_TOKEN"]
     updater = Updater(token = tg_token)
     print(updater.bot.get_me())
-    updater.bot.send_message(chat_id="@bot_programm", text="Здесь будут фотографии NASA")
+   # updater.bot.send_message(chat_id="@bot_programm", text="Здесь будут фотографии NASA")
+    with open("space.jpg", "rb") as file:
+        photo=file
+        updater.bot.send_photo(chat_id="@bot_programm", photo=photo)
+
     
 
 
