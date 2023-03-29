be7dc3953b10528ca981e862ddd7dc71f5d25652
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2023-03-29 17:49:35 +0300

    add argparse

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,28 +1,22 @@
 import os.path
+import argparse
 from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
 
-def get_image(filename, url):
+
+def get_url_image(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
-    print(response)
-    with open (filename, "wb") as file:
-        file.write(response.content)
+    links_images = response.json()["links"]["flickr"]["original"]
+    return links_images
 
 
-def get_url_image():
-    url = "https://api.spacexdata.com/v5/launches"
-    response = requests.get(url)
-    response.raise_for_status()
-    list_links_images = [link["links"]["flickr"]["original"] for link in response.json()]
-    return list_links_images
-
-
-def fetch_spacex_all_launches(list_links_images, folder="images"):
+def fetch_spacex_all_launches(links_images, folder="images"):
     Path(folder).mkdir(parents=True, exist_ok=True)
-    for number, link in enumerate(list_links_images):
+    for number, link in enumerate(links_images):
         response = requests.get(link)
         response.raise_for_status()
         file_name = f"image{number}.jpeg"
@@ -33,14 +27,18 @@ def fetch_spacex_all_launches(list_links_images, folder="images"):
 
 def main():
     load_dotenv()
+    parser = argparse.ArgumentParser(
+        description="Загружает фотографии SpaceX"
+    )
+    parser.add_argument('--launch_id', help='Id запуска', default="5eb87d42ffd86e000604b384")
+    args = parser.parse_args()
+    launch_id = args.launch_id
+
     Path("images").mkdir(parents=True, exist_ok=True)
   
-    filename = "images/hubble.jpeg"
-    url = "https://api.spacexdata.com/v5/launches/latest"
-  
-    get_image(filename, url)
-    list_links_images = get_url_image()
-    fetch_spacex_all_launches(list_links_images)
+    links_images = get_url_image(launch_id)
+    fetch_spacex_all_launches(links_images)
+
 
 if __name__ == "__main__":
     main()
