708b0e2fffa5b71126b127042e35e32a44e2dd68
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2023-04-10 17:16:46 +0300

    edit function

--- download_image.py
+++ download_image.py
@@ -1,7 +1,8 @@
 import requests
 
-def download_image(file_path, url_images):
-    response = requests.get(url_images)
+def download_image(file_path, url_images, params=None):
+
+    response = requests.get(url_images, params=params)
     response.raise_for_status()
   
     with open (file_path, "wb") as file:
