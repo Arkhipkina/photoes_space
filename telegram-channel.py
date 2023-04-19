99d704903fe8092b90b137b39a69640682625991
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2023-04-19 17:04:48 +0300

    Update telegram-channel.py

--- telegram-channel.py
+++ telegram-channel.py
@@ -16,14 +16,14 @@ def get_random_path_image(folder="images"):
     return path_images
 
 
-def get_piblish_photoes(updater, delay_time, chat_id):
+def get_piblish_photoes(updater, delay_time, tg_chat_id):
     while True:
         try:
             path_images = get_random_path_image()
             for path_image in path_images:
                 with open(path_image, "rb") as file:
                     photo=file
-                    updater.bot.send_photo(chat_id, photo=photo)
+                    updater.bot.send_photo(tg_chat_id, photo=photo)
                 sleep(delay_time)
         except requests.exceptions.ConnectionError():
             sleep(20)
@@ -32,11 +32,11 @@ def get_piblish_photoes(updater, delay_time, chat_id):
 
 def main():
     load_dotenv()
-    chat_id = os.environ["CHAT_ID"]
+    tg_chat_id = os.environ["tG_CHAT_ID"]
     tg_token = os.environ["TG_TOKEN"]
     updater = Updater(token = tg_token)
     delay_time = os.getenv("DELAY_TIME", default=14400)
-    get_piblish_photoes(updater, int(delay_time), chat_id)
+    get_piblish_photoes(updater, int(delay_time), tg_chat_id)
     
 
 
